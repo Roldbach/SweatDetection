61bd82550041b194d5cc8e9fc0fc7e5b85c08ce2
Roldbach <roldbach@yahoo.com>
2022-03-14 18:30:46 +0000

    first commit

---
+++ Test.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from DataHandling.DataBase import DataBase
+'''
+test2="2022/03/13 23:20:41"
+time=datetime.strptime(test2,"%Y/%m/%d %H:%M:%S")
+print(time)
+print(type(time))
+'''
+
+test=DataBase()
+print(test.Glucose)
